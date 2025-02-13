24c7fc066630e1ee9fc8026296ac1d88e9d0dfe0
unknown <the6861@naver.com>
2025-02-13 18:38:15 +0900

    antd 기본 적용

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
 import "./App.css";
 import MainPageComponent from "./main/index.js";
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Link, useHistory } from "react-router-dom";
 import UploadPage from "./upload/index.js";
 import ProductPage from "./product/index.js";
+import { Button } from "antd";
+import { DownloadOutlined } from "@ant-design/icons";
 
 function App() {
+  const history = useHistory();
   return (
     <div>
       <div id="header">
         <div id="header-area">
-          <img src="/images/icons/logo.png" />
+          <Link to="/">
+            <img src="/images/icons/logo.png" />
+          </Link>
+          <Button
+            size="large"
+            onClick={function () {
+              history.push("/upload");
+            }}
+            icon={<DownloadOutlined />}
+          >
+            상품업로드
+          </Button>
         </div>
       </div>
       <div id="body">
